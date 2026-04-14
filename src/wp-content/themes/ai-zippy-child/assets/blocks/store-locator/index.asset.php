72e246d5808012b097bd5ee2a21573009e7ff0eb
vince-8 <vince.vinhsam@ant-intl.com>
2026-04-14 18:56:14 +0700

    Adjust theme variables

--- src/wp-content/themes/ai-zippy-child/assets/blocks/store-locator/index.asset.php
+++ src/wp-content/themes/ai-zippy-child/assets/blocks/store-locator/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f0fc7287536ed7cf4289');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '32b94728a53a79b77e5c');
