ade28d2860f8dcfb6417a4fdfe0876f8cb6333f2
vince-8 <vince.vinhsam@ant-intl.com>
2026-04-15 17:53:45 +0700

    Fix some mobile issues

--- src/wp-content/themes/ai-zippy-child/assets/blocks/site-footer/index.asset.php
+++ src/wp-content/themes/ai-zippy-child/assets/blocks/site-footer/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '50882559c8615e76e0d0');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b8802e55ec2f90ca64c2');
--- src/wp-content/themes/ai-zippy-child/assets/blocks/store-locator/index.asset.php
+++ src/wp-content/themes/ai-zippy-child/assets/blocks/store-locator/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '32b94728a53a79b77e5c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '596f936144eca44b5480');
